d057b9a5106037ec3a9bebb957f82bfea85919cf
DigruttolaGabriel <digruttolagabriel@gmail.com>
2017-06-23 16:03:24 -0300

    First Commit

---
+++ DemoGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = 2 + 4;
+            //Alta demo, papa 
+        }
+    }
+}
